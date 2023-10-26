e5d420f725316806e2ebbdb54d6d3b75e8ece75e
Butter Dog CEO <106115826+butterdogco@users.noreply.github.com>
2023-10-26 12:51:52 -0700

    Update | New user-submitted articles.

--- js/articlesDir.js
+++ js/articlesDir.js
@@ -1,72 +1,102 @@
+// This is the directory of every article in Wikabedia, every article has an ID (the number it is down the list)
+
 const data = [
   {
-    title: "wrld histry",
+    title: "wrld histry", // 1
     desc: "te worlds histry",
     image: "img/wrld-histry.jpeg",
     link: "Articles/wrld-histry.html",
     place: "items"
   },
   {
-    title: "how to read engis",
+    title: "how to read engis", // 2
     desc: "tells you how to read",
     image: "img/how-to-read.jpeg",
     link: "Articles/how-to-read.html",
     place: "items"
   },
   {
-    title: "how to typ letrs",
+    title: "how to typ letrs", // 3
     desc: "tels you how to typ",
     image: "img/how-to-type.jpeg",
     link: "Articles/how-to-type.html",
     place: "items"
   },
   {
-    title: "how fish asigments erly",
+    title: "how fish asigments erly", // 4
     desc: "help to finsh asignment erly",
     image: "img/how-to-fish-asigments-erly.jpeg",
     link: "Articles/how-to-fish-asignment.html",
     place: "items"
   },
   {
-    title: "whats the date",
+    title: "whats the date", // 5
     desc: "the date",
     image: "img/whats-the-date.jpeg",
     link: "Articles/whats-the-date.html",
     place: "items"
   },
   {
-    title: "how eat eggplant",
+    title: "how eat eggplant", // 6
     desc: "lern to eat superfast",
     image: "img/how-eat-eggplant.jpeg",
     link: "Articles/how-to-eat-eggplant.html",
     place: "items"
   },
   {
-    title: "how see",
+    title: "how see", // 7
     desc: "how light work",
     image: "img/how-see.jpeg",
     link: "Articles/how-see.html",
     place: "items"
   },
   {
-    title: "creating a piep bom",
+    title: "creating a piep bom", // 8
     desc: "step by sep tutorial",
     image: "img/creating-a-piep-bom.jpg",
     link: "Articles/Pipe-bomb.html",
     place: "items"
   },
   {
-    title: "hiding evidence",
+    title: "hiding evidence", // 9
     desc: "tep by setp",
     image: "img/hiding-evidence.jpeg",
     link: "Articles/hide-evidence.html",
     place: "items"
   },
   {
-    title: "how to ge hot",
+    title: "how to ge hot", // 10
     desc: "totoial set by ep",
     image: "img/how-to-get-hot.jpeg",
     link: "Articles/how-to-get-hot.html",
+    place: "items"
+  },
+  {
+    title: "how to b humen", // 11
+    desc: "reel tutoieal",
+    image: "img/how-to-be-human.jpg",
+    link: "Articles/how-to-get-hot.html",
+    place: "items"
+  },
+  {
+    title: "how to do maht", // 12
+    desc: "maht",
+    image: "img/how-to-do-math.jpg",
+    link: "Articles/how-to-do-math.html",
+    place: "items"
+  },
+  {
+    title: "trens", // 13
+    desc: "choo chooooo",
+    image: "img/noah-trens.png",
+    link: "Articles/noah-trens.html",
+    place: "items"
+  },
+  {
+    title: "da buterdog", // 14
+    desc: "the ceo",
+    image: "img/butterdog.ico",
+    link: "Articles/nolan-da-buterdog.html",
     place: "items"
   },
 ];
