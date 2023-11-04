171223095c002022a370f06014ab5e45c3714503
Butter Dog CEO <106115826+butterdogco@users.noreply.github.com>
2023-11-03 21:33:46 -0700

    upDAETE!! | artical an siezing!!!!

--- js/articlesDir.js
+++ js/articlesDir.js
@@ -7,7 +7,7 @@ const data = [
     image: "img/wrld-histry.jpeg",
     link: "Articles/wrld-histry.html",
     place: "items",
-    tags: "world wrld wold history histry histoy 2023 2021 2020 19 18 16 15 14 earth erth eath eart",
+    tags: "world wrld wold history histry histoy 2023 2021 2020 19 18 16 15 14 earth erth eath eart keegan",
     text: "yeer is 2023. peeple did tings!"
   },
   {
@@ -16,7 +16,7 @@ const data = [
     image: "img/how-to-read.jpeg",
     link: "Articles/how-to-read.html",
     place: "items",
-    tags: "read english red read raed rad engis englis englih language anguage",
+    tags: "read english red read raed rad engis englis englih language anguage keegan",
     text: "1. read /n 2. finisdhed leson todya!!11"
   },
   {
@@ -25,7 +25,7 @@ const data = [
     image: "img/how-to-type.jpeg",
     link: "Articles/how-to-type.html",
     place: "items",
-    tags: "keyboard keys letters leter letter keybord kebord keboard type typing"
+    tags: "keyboard keys letters leter letter keybord kebord keboard type typing keegan"
   },
   {
     title: "how fish asigments erly", // 4
@@ -33,7 +33,7 @@ const data = [
     image: "img/how-to-fish-asigments-erly.jpeg",
     link: "Articles/how-to-fish-asignment.html",
     place: "items",
-    tags: "finish finis finihs assignment asignment work classwork homework quiz quizes tests"
+    tags: "finish finis finihs assignment asignment work classwork homework quiz quizes tests keegan"
   },
   {
     title: "whats the date", // 5
@@ -41,7 +41,7 @@ const data = [
     image: "img/whats-the-date.jpeg",
     link: "Articles/whats-the-date.html",
     place: "items",
-    tags: "date time daet tiem calender calendar what's ical what's the date what's the daet"
+    tags: "date time daet tiem calender calendar what's ical what's the date what's the daet keegan"
   },
   {
     title: "how eat eggplant", // 6
@@ -49,7 +49,7 @@ const data = [
     image: "img/how-eat-eggplant.jpeg",
     link: "Articles/how-to-eat-eggplant.html",
     place: "items",
-    tags: "food eggplant eat egplant eggplan egplan eating edible"
+    tags: "food eggplant eat egplant eggplan egplan eating edible keegan"
   },
   {
     title: "how see", // 7
@@ -57,7 +57,7 @@ const data = [
     image: "img/how-see.jpeg",
     link: "Articles/how-see.html",
     place: "items",
-    tags: "light physics science see lights bulb electricity"
+    tags: "light physics science see lights bulb electricity keegan"
   },
   {
     title: "creating a piep bom", // 8
@@ -65,7 +65,7 @@ const data = [
     image: "img/creating-a-piep-bom.jpg",
     link: "Articles/Pipe-bomb.html",
     place: "items",
-    tags: "weapon pipe bomb damage tnt explosive boom"
+    tags: "weapon pipe bomb damage tnt explosive boom keegan"
   },
   {
     title: "hiding evidence", // 9
@@ -73,7 +73,7 @@ const data = [
     image: "img/hiding-evidence.jpeg",
     link: "Articles/hide-evidence.html",
     place: "items",
-    tags: "evidence hiding forest legal trouble"
+    tags: "evidence hiding forest legal trouble keegan"
   },
   {
     title: "how to ge hot", // 10
@@ -81,7 +81,7 @@ const data = [
     image: "img/how-to-get-hot.jpeg",
     link: "Articles/how-to-get-hot.html",
     place: "items",
-    tags: "hot tempurature lava heat"
+    tags: "hot tempurature lava heat keegan"
   },
   {
     title: "how to b humen", // 11
@@ -89,7 +89,7 @@ const data = [
     image: "img/how-to-be-human.jpg",
     link: "Articles/how-to-be-human.html",
     place: "items",
-    tags: "human dog animal mammal pet person humanoid"
+    tags: "human dog animal mammal pet person humanoid keegan"
   },
   {
     title: "how to do maht", // 12
@@ -97,7 +97,7 @@ const data = [
     image: "img/how-to-do-math.jpg",
     link: "Articles/how-to-math-do.html",
     place: "items",
-    tags: "math algebra sin cos tan calculator maht meth"
+    tags: "math algebra sin cos tan calculator maht meth keegan"
   },
   {
     title: "trens", // 13
@@ -105,7 +105,7 @@ const data = [
     image: "img/noah-trens.png",
     link: "Articles/noah-trens.html",
     place: "items",
-    tags: "train whistle choo choo train"
+    tags: "train whistle choo choo train noah"
   },
   {
     title: "da buterdog", // 14
@@ -113,7 +113,7 @@ const data = [
     image: "img/butterdog.jpg",
     link: "Articles/nolan-da-buterdog.html",
     place: "items",
-    tags: "ceo butterdog buterdog "
+    tags: "ceo butterdog buterdog nolan"
   },
   {
     title: "wat da dog", // 15
@@ -121,7 +121,7 @@ const data = [
     image: "img/wat-da-dog.jpg",
     link: "Articles/wat-da-dog.html",
     place: "items",
-    tags: "dog woof bark doggy doggie"
+    tags: "dog woof bark doggy doggie keegan"
   },
   {
     title: "Noe nane givn1!", // 16
@@ -129,7 +129,7 @@ const data = [
     image: "img/kai-I fogot.JPG",
     link: "Articles/kai-I fogot.html",
     place: "items",
-    tags: "i don no idk i don't know dont confused random potato"
+    tags: "i don no idk i don't know dont confused random potato kai"
   },
   {
     title: "E", // 17
@@ -137,7 +137,7 @@ const data = [
     image: "img/kai-e.jpg",
     link: "Articles/kai-e.html",
     place: "items",
-    tags: "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE 'e' letter leter alphabet abc's abcs abcdefghijklmnopqrstuvwxyz"
+    tags: "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE 'e' letter leter alphabet abc's abcs abcdefghijklmnopqrstuvwxyz kai"
   },
   {
     title: "us histrey", // 18
@@ -153,7 +153,7 @@ const data = [
     image: "img/nolan-b-ritch.jpg",
     link: "Articles/nolan-b-ritch.html",
     place: "items",
-    tags: "money cash how to be rich bank of america"
+    tags: "money cash how to be rich bank of america nolan"
   },
     {
     title: "top 5 positions", // 20
@@ -161,7 +161,7 @@ const data = [
     image: "img/nolan-top-5-positions.png",
     link: "Articles/nolan-top-5-positions.html",
     place: "items",
-    tags: "sit down sitting down seating chair positions"
+    tags: "sit down sitting down seating chair positions nolan"
   },
   {
     title: "How 2 Invest", // 21
@@ -169,7 +169,7 @@ const data = [
     image: "img/nolan-how-2-invest.jpg",
     link: "Articles/nolan-how-2-invest.html",
     place: "items",
-    tags: "invest money rich ritch investing money cash dollar $"
+    tags: "invest money rich ritch investing money cash dollar $ nolan"
   },
   {
     title: "wrld histry 2: rematserd", // 22
@@ -177,7 +177,7 @@ const data = [
     image: "img/nolan-world-history-2.jpeg",
     link: "Articles/nolan-world-history-2.html",
     place: "items",
-    tags: "world history earth lore world lore"
+    tags: "world history earth lore world lore nolan"
   },
   {
     title: "Your answer", // 23
@@ -185,6 +185,36 @@ const data = [
     image: "img/nolan-your-answer.png",
     link: "Articles/nolan-your-answer.html",
     place: "items",
-    tags: "your answer useless article"
+    tags: "your answer useless article nolan"
+  },
+  {
+    title: "How to draw good", // 24
+    desc: "how 2 draw",
+    image: "img/kai-how-to-draw.PNG",
+    link: "Articles/kai-how-to-draw.html",
+    place: "items",
+    tags: "draw artist drawing tutorial kai"
+  },
+  {
+    title: "idk", // 25
+    desc: "imbouttobust",
+    image: "img/alexis-idk.png",
+    link: "Articles/alexis-idk.html",
+    place: "items",
+    tags: "idk alexis"
   },
 ];
+
+// function sheetData(c1,c2) {
+//   const spreadsheetId = '1LlL8mrSXTTV6qHOkUKd57oVb0uZATq037Wg4ltlDreg';
+//   const range = 'wikabedia!' + c1/* + ":" + c2*/; // Specify the sheet name and range
+
+//   fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=AIzaSyBie4PasgrxYkF7LRl8zcCGUsnBnwZ8pWE`)
+//     .then(response => response.json())
+//     .then(data => {
+//       // Process the data
+//       // alert(data.values);
+//       // document.getElementById("logoText").innerHTML = JSON.stringify(data);
+//       return data.values;
+//     });
+// }
