27e1ab008653fd55b0c1e6313073c8f7e5a98883
Butter Dog CEO <106115826+butterdogco@users.noreply.github.com>
2023-11-09 14:13:42 -0800

    uPADET!! | beter serch system!!!!!

--- js/search.js
+++ js/search.js
@@ -42,52 +42,53 @@ function site(url) {
 function search(mode) {
   var searchItemsElement = document.getElementById("searchItems");
   searchItemsElement.innerHTML = "";
-  
+
   // Get the URL of the current page
   var url = new URL(window.location);
+
   // Get the search query parameter from the URL
+  var searchQuery;
   if (mode === "load") {
-    var searchQuery = url.searchParams.get("search");
-  } else {
-    if (mode === "page") {
-      searchQuery = document.getElementById('searchBox').value;
-      window.location.href = "?search=" + searchQuery;
-    } else {
-      if (mode === "page2") {
-        searchQuery = document.getElementById('searchBox2').value;
-        window.location.href = "?search=" + searchQuery;
-      }
-    }
+    searchQuery = url.searchParams.get("search");
+  } else if (mode === "page") {
+    searchQuery = document.getElementById('searchBox').value;
+    window.location.href = "?search=" + searchQuery;
+  } else if (mode === "page2") {
+    searchQuery = document.getElementById('searchBox2').value;
+    window.location.href = "?search=" + searchQuery;
   }
   
-  if (url.searchParams.get("search") === "undefined") {
-    var searchQuery = ' ';
+  if (searchQuery === undefined) {
+    searchQuery = ' ';
   }
-  
 
   // Perform the search operation if a search query exists
   if (searchQuery) {
-    document.getElementById('queryText').innerHTML = ''.concat("serched 4 '", searchQuery, "'");
-    
+    document.getElementById('queryText').innerHTML = ''.concat("searched 4 '", searchQuery, "'");
+
     var found = false;
 
     // Use the search query to perform a search or any other operations
     // For example, search through a list of items
     data.forEach(item => {
-      if (item.title.includes(searchQuery) || item.desc.includes(searchQuery) || item.tags.includes(searchQuery)) {
+      var words = searchQuery.split(' ');
+      var match = words.some(word => item.title.includes(word) || item.desc.includes(word) || item.tags.includes(word));
+      if (match) {
         found = true;
         load(item.title, item.desc, item.image, item.link, "searchItems");
       }
-      
-      // if (document.getElementById('searchItems').hasChildNodes() === false && found === false) {
-      //   const e = document.createElement('h4');
-      //   e.innerHTML = "sory can no find da '" + searchQuery + "'";
-      //   e.setAttribute("id","notFound");
-      //   document.getElementById('searchItems').appendChild(e);
-      // } else {
-      //   document.getElementById('notFound').remove();
-      // }
     });
+
+    if (document.getElementById('searchItems').hasChildNodes() === false && found === false) {
+      const e = document.createElement('h4');
+      e.innerHTML = "sory, no find anyting like dis: '" + searchQuery + "'";
+      e.setAttribute("id","notFound");
+      document.getElementById('searchItems').appendChild(e);
+    } else {
+      if (document.getElementById('notFound')) {
+        document.getElementById('notFound').remove();
+      }
+    }
   }
 }
 
