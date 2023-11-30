1a50efa7bb2e90d933fd96fb21f7c7571fb46ce4
Butter Dog CEO <106115826+butterdogco@users.noreply.github.com>
2023-11-30 08:42:04 -0800

    update | bug fix

--- js/recomend.js
+++ js/recomend.js
@@ -1,22 +1,26 @@
-function createDefaults() {
+async function createDefaults() {
   try {
-     // Shuffle the data array
-  const shuffledData = data.slice();
-  for (let i = shuffledData.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [shuffledData[i], shuffledData[j]] = [shuffledData[j], shuffledData[i]];
-  }
-
-  // Get the first 5 items from the shuffled array
-  const randomItems = shuffledData.slice(0, 5);
-
-  // Call the load function for each of the random items
-  randomItems.forEach(item => {
-    if (item.desc === null) {
-      item.desc = "No description";
+    while (!done) {
+      await new Promise(resolve => setTimeout(resolve, 150)); // Wait for 100 milliseconds
     }
-    load(item.title, item.desc, item.image, item.link, "recomend");
-  });
+    
+     // Shuffle the data array
+    const shuffledData = data.slice();
+    for (let i = shuffledData.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledData[i], shuffledData[j]] = [shuffledData[j], shuffledData[i]];
+    }
+    
+    // Get the first 5 items from the shuffled array
+    const randomItems = shuffledData.slice(0, 5);
+  
+    // Call the load function for each of the random items
+    randomItems.forEach(item => {
+      if (item.desc === null) {
+        item.desc = "No description";
+      }
+      load(item.title, item.desc, item.image, item.link, "recomend");
+    });
   } catch (error) {
     alert(error)
   }
