60d7190298fd6652069e2490ab55297c5c4246e5
ButterDogCEO <100174775+butterdogceo@users.noreply.github.com>
2024-05-17 14:12:54 -0700

    update | fixed

--- js/main.js
+++ js/main.js
@@ -6,8 +6,11 @@ errorDiv.style.top = "50%";
 errorDiv.style.left = "50%";
 errorDiv.style.transform = "translate(-50%, -50%)";
 errorDiv.style.border = "2px solid gray";
+errorDiv.style.width = "90vw";
+errorDiv.style.height = "90vh";
+errorDiv.style.zIndex = "999999999999999999999999999999999999999999999999";
 
-const notice = document.createElement("p")
+const notice = document.createElement("p");
 notice.innerText = "Debug Console | CTRL + Y to toggle";
 notice.style.paddingBottom = "5px";
 notice.style.paddingTop = "5px";
@@ -18,28 +21,28 @@ errorDiv.appendChild(notice);
 document.body.appendChild(errorDiv);
 
 function createLog(msg, url, linenumber) {
-    const e = document.createElement("p");
-    e.innerHTML = `Error: ${msg}<br>URL: ${url}<br>Line: ${linenumber}`;
-    e.style.border = "2px solid gray";
-    e.style.background = "white";
-    e.style.width = "100%";
-    e.style.padding = "3px";
-    e.style.color = "rgb(230,20,20)";
-    e.style.fontFamily = "monospace";
-    errorDiv.appendChild(e);
+  const e = document.createElement("p");
+  e.innerHTML = `Error: ${msg}<br>URL: ${url}<br>Line: ${linenumber}`;
+  e.style.background = "rgba(230,20,20,0.1)";
+  e.style.width = "100%";
+  e.style.paddingBottom = "5px";
+  e.style.paddingTop = "5px";
+  e.style.color = "rgb(230,20,20)";
+  e.style.fontFamily = "monospace";
+  errorDiv.appendChild(e);
 }
 
 window.onerror = function(msg, url, linenumber) {
-    createLog(msg, url, linenumber);
-    return true;
-}
+  createLog(msg, url, linenumber);
+  return true;
+};
 
-document.body.addEventListener("onkeydown", (event) => {
-    if (event.keyCode === 89 && event.ctrlKey) {
-        if (errorDiv.style.display === "block") {
-            errorDiv.style.display = "none";
-        } else {
-            errorDiv.style.display = "block";
-        }
+document.body.onkeydown = function(event) {
+  if (event.keyCode === 89 && event.ctrlKey) {
+    if (errorDiv.style.display === "block") {
+        errorDiv.style.display = "none";
+    } else {
+        errorDiv.style.display = "block";
     }
-});
+  }
+};
